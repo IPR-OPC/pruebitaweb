212c19cc5748be03bbe7733f9173523858d4f923
Juan Martí <jmarti.dev@gmail.com>
2023-07-25 15:03:29 -0400

    Genesis

---
+++ pruebitaweb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace pruebitaweb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
